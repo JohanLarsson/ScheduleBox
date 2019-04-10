0bb1a3cea2253c25926cd8fb80e6f8085bf7f34c
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-10 11:29:01 +0200

    Remove comments from template project.

--- ScheduleBox/Startup.cs
+++ ScheduleBox/Startup.cs
@@ -18,12 +18,9 @@ namespace ScheduleBox
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
@@ -34,7 +31,6 @@ namespace ScheduleBox
             services.AddCors();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -61,8 +57,6 @@ namespace ScheduleBox
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
