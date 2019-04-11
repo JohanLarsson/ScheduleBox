912f282315052b3c95dac460a6ac4619e569d826
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-11 09:16:12 +0200

    Use strings for date.

--- ScheduleBox/ClientApp/src/app/schedule/schedule.component.ts
+++ ScheduleBox/ClientApp/src/app/schedule/schedule.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ScheduleComponent implements OnInit, OnDestroy {
   json = 'Empty';
-  private _date = new Date();
+  private _date: string;
   private dateSub: any;
 
   constructor(
@@ -18,27 +18,29 @@ export class ScheduleComponent implements OnInit, OnDestroy {
     private http: HttpClient,
   ) { }
 
-  
-  public get date() : Date {
+
+  public get date() : string {
     return this._date;
   }
-    
-  public set date(v : Date) {
-    // if passed as string from the input
-    v = new Date(v);
-    this._date = v;
-    const isoDate = v.toISOString().substring(0, 10);
+
+  public set date(v : string) {
+    const isoDate = this.getIsoDate(v);
+    this._date = isoDate;
     this.router.navigate([`/${isoDate}`]);
     this.json = '';
     this.http.get<string>(`${document.getElementsByTagName('base')[0].href}api/schedules/${isoDate}`)
              .subscribe(x => this.json = JSON.stringify(x));
   }
-  
+
   ngOnInit(): void {
-    this.dateSub = this.route.paramMap.subscribe(x => this.date = new Date(x.get('date')));
+    this.dateSub = this.route.paramMap.subscribe(x => this.date = this.getIsoDate(x.get('date')));
   }
 
   ngOnDestroy() {
     this.dateSub.unsubscribe();
+  }
+
+  getIsoDate(text: string): string {
+    return new Date(text).toISOString().substring(0, 10);
   }
 }
