3fe0d629f69576b1d269621df8bf64cfe555b354
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-11 09:59:41 +0200

    Use query parameter for attendees.

--- ScheduleBox/ClientApp/src/app/book-standup/book-standup.component.ts
+++ ScheduleBox/ClientApp/src/app/book-standup/book-standup.component.ts
@@ -10,7 +10,7 @@ import { BookStandupResponse } from './BookStandupResponse';
 export class BookStandupComponent implements OnInit, OnDestroy {
   response: BookStandupResponse;
   private _attendees: number | null;
-  private _date: string;
+  private _isoDate: string;
   private sub: any;
 
   constructor(
@@ -20,13 +20,13 @@ export class BookStandupComponent implements OnInit, OnDestroy {
   ){ }
 
   public get date(): string {
-    return this._date;
+    return this._isoDate;
   }
 
   public set date(v : string) {
     const isoDate = this.getIsoDate(v);
-    this._date = isoDate;
-    this.router.navigate([`/book-standup/${isoDate}`]);
+    this._isoDate = isoDate;
+    this.navigate();
   }
 
   public get attendees(): number | null {
@@ -35,12 +35,13 @@ export class BookStandupComponent implements OnInit, OnDestroy {
 
   public set attendees(v: number | null) {
     this._attendees = v;
+    this.navigate();
   }
 
   ngOnInit(): void {
     this.sub = this.route.paramMap.subscribe(map => {
       const isoDate = this.getIsoDate(map.get('date'));
-      this._date = isoDate;
+      this._isoDate = isoDate;
       this.response = null;
       this.http.get<BookStandupResponse>(`${document.getElementsByTagName('base')[0].href}api/schedules/${isoDate}`)
                .subscribe(x => this.response = x);
@@ -53,5 +54,9 @@ export class BookStandupComponent implements OnInit, OnDestroy {
 
   getIsoDate(text: string): string {
     return new Date(text).toISOString().substring(0, 10);
+  }
+
+  private navigate() {
+    this.router.navigate([`/book-standup/${this._isoDate}`], { queryParams: { attendees: this.attendees } });
   }
 }
