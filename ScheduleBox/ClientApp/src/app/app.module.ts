55204af30b57218a617a7ee8421facc705719f67
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-12 13:32:38 +0200

    material module

--- ScheduleBox/ClientApp/src/app/app.module.ts
+++ ScheduleBox/ClientApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookStandupComponent } from './book-standup/book-standup.component';
 import { ChartComponent } from './chart/chart.component';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
@@ -15,9 +17,11 @@ import { ChartComponent } from './chart/chart.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    MaterialModule,
   ],
   providers: [ ],
   bootstrap: [AppComponent]
---
+++ ScheduleBox/ClientApp/src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatDatepickerModule,
+  ],
+  exports: [
+    MatDatepickerModule,
+  ]
+})
+export class MaterialModule { }
