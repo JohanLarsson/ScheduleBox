32c01602c616ecc68f36d8d03b1259457ecb27d9
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-12 13:39:11 +0200

    Import MaterialIconModule

--- ScheduleBox/ClientApp/src/app/shared/material.module.ts
+++ ScheduleBox/ClientApp/src/app/shared/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerModule, MaterialIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     MatDatepickerModule,
+    MaterialIconModule,
   ],
   exports: [
     MatDatepickerModule,
+    MaterialIconModule,
   ]
 })
 export class MaterialModule { }
