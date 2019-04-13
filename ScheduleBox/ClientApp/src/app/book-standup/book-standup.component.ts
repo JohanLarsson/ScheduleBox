a12844bffeae1f1dc961beb3e270b1efee4d202c
JohanLarsson <johan_gavle1@hotmail.com>
2019-04-13 14:59:20 +0200

    Fix ts lint warnings.

--- ScheduleBox/ClientApp/src/app/book-standup/book-standup.component.ts
+++ ScheduleBox/ClientApp/src/app/book-standup/book-standup.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { map, distinctUntilChanged } from "rxjs/operators";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, distinctUntilChanged } from 'rxjs/operators';
 import { ScheduleService } from '../schedule/schedule.service';
 import { combineLatest } from 'rxjs';
 
 @Component({
-  templateUrl: "./book-standup.component.html",
-  styleUrls: ["./book-standup.component.scss"]
+  templateUrl: './book-standup.component.html',
+  styleUrls: ['./book-standup.component.scss']
 })
 export class BookStandupComponent implements OnInit, OnDestroy {
   private dateParameterSubcription: any;
@@ -22,13 +22,13 @@ export class BookStandupComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.dateParameterSubcription = this.route.paramMap
       .pipe(
-        map(paramMap => new Date(paramMap.get("date"))),
+        map(paramMap => new Date(paramMap.get('date'))),
         distinctUntilChanged())
       .subscribe(date => this.scheduleService.date = date);
 
     this.attendeesQueryParameterSubcription = this.route.queryParamMap
       .pipe(
-        map(paramMap => paramMap.get("attendees")),
+        map(paramMap => paramMap.get('attendees')),
         distinctUntilChanged())
       .subscribe(x => this.scheduleService.attendees = x ? +x : null);
 
