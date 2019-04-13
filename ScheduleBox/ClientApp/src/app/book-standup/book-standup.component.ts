7c1911bb4092cd7efd46f7c6642b07f80e8a274f
Johan Larsson <johan_gavle1@hotmail.com>
2019-04-13 12:43:52 +0200

    WIP mat-datepicker
    Broken when daylight saving time.

--- ScheduleBox/ClientApp/src/app/book-standup/book-standup.component.ts
+++ ScheduleBox/ClientApp/src/app/book-standup/book-standup.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { SchedulesResponse } from "../schedule/SchedulesResponse";
 import { map, distinctUntilChanged } from "rxjs/operators";
 import { ScheduleService } from '../schedule/schedule.service';
+import { combineLatest } from 'rxjs';
 
 @Component({
   templateUrl: "./book-standup.component.html",
   styleUrls: ["./book-standup.component.scss"]
 })
 export class BookStandupComponent implements OnInit, OnDestroy {
-  private dateSubcription: any;
-  private attendeesSubcription: any;
+  private dateParameterSubcription: any;
+  private attendeesQueryParameterSubcription: any;
+  private navigateSubscription: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -18,52 +19,28 @@ export class BookStandupComponent implements OnInit, OnDestroy {
     public scheduleService: ScheduleService
   ) { }
 
-  public get date(): string {
-    return this.scheduleService.isoDate;
-  }
-
-  public set date(v: string) {
-    this.scheduleService.isoDate = this.getIsoDate(v);
-    this.navigate();
-  }
-
-  public get attendees(): number | null {
-    return this.scheduleService.attendees;
-  }
-
-  public set attendees(v: number | null) {
-    this.scheduleService.attendees = v;
-    this.navigate();
-  }
-
   ngOnInit(): void {
-    this.dateSubcription = this.route.paramMap
+    this.dateParameterSubcription = this.route.paramMap
       .pipe(
-        map(paramMap => this.getIsoDate(paramMap.get("date"))),
+        map(paramMap => new Date(paramMap.get("date"))),
         distinctUntilChanged())
-      .subscribe(isoDate => {
-        this.scheduleService.isoDate = isoDate;
-      });
+      .subscribe(date => this.scheduleService.date = date);
 
-    this.attendeesSubcription = this.route.queryParamMap
+    this.attendeesQueryParameterSubcription = this.route.queryParamMap
       .pipe(
         map(paramMap => paramMap.get("attendees")),
         distinctUntilChanged())
       .subscribe(x => this.scheduleService.attendees = x ? +x : null);
+
+    this.navigateSubscription = combineLatest(
+      this.scheduleService.isoDate,
+      this.scheduleService.attendeesObservable)
+      .subscribe(x => this.router.navigate([`/book-standup/${x[0]}`], { queryParams: { attendees: x[1] } }));
   }
 
   ngOnDestroy() {
-    this.dateSubcription.unsubscribe();
-    this.attendeesSubcription.unsubscribe();
-  }
-
-  getIsoDate(text: string): string {
-    return new Date(text).toISOString().substring(0, 10);
-  }
-
-  private navigate() {
-    this.router.navigate([`/book-standup/${this.scheduleService.isoDate}`], {
-      queryParams: { attendees: this.attendees }
-    });
+    this.dateParameterSubcription.unsubscribe();
+    this.attendeesQueryParameterSubcription.unsubscribe();
+    this.navigateSubscription.unsubscribe();
   }
 }
--- ScheduleBox/ClientApp/src/app/schedule/schedule.service.ts
+++ ScheduleBox/ClientApp/src/app/schedule/schedule.service.ts
@@ -2,36 +2,59 @@ import { Injectable } from '@angular/core';
 import { SchedulesResponse } from './SchedulesResponse';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduleService {
-  private readonly _response = new BehaviorSubject<SchedulesResponse>(null);
   error: string | null;
-  attendees: number | null;
-  private _isoDate: string | null;
+  private readonly _response = new BehaviorSubject<SchedulesResponse>(null);
+  private _date: Date;
+  private _isoDate = new BehaviorSubject<string>('');
+  private _attendees = new BehaviorSubject<number | null>(null);
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  public get isoDate(): string {
-    return this._isoDate;
+  constructor(http: HttpClient
+  ) { 
+    this._isoDate.pipe(
+      distinctUntilChanged())
+      .subscribe(isoDate =>{
+        this._response.next(null);
+        this.error = null;
+        http
+          .get<SchedulesResponse>(`${document.getElementsByTagName("base")[0].href}api/schedules/${isoDate}`)
+          .subscribe(
+            response => this._response.next(response),
+            error => this.error = error.error);
+      })
   }
 
-  public set isoDate(v: string) {
-    this._isoDate = v;
-    this._response.next(null);
-    this.error = null;
-    this.http
-      .get<SchedulesResponse>(`${document.getElementsByTagName("base")[0].href}api/schedules/${v}`)
-      .subscribe(
-        response => this._response.next(response),
-        error => this.error = error.error);
+  public get date(): Date {
+    return this._date;
+  }
+
+  public set date(v: Date) {
+    this._date = v;
+    this._isoDate.next(v.toISOString().substring(0, 10))
+  }
+
+  public get isoDate(): Observable<string> {
+    return this._isoDate.pipe(distinctUntilChanged());
+  }
+
+  public get attendees() : number | null {
+    return this._attendees.value;
   }
   
-  public get response() : Observable<SchedulesResponse> {
+  public get attendeesObservable() : Observable<number | null> {
+    return this._attendees.pipe(distinctUntilChanged());
+  }
+
+  public set attendees(v : number | null) {
+    this._attendees.next(v);
+  }
+
+  public get response(): Observable<SchedulesResponse> {
     return this._response.asObservable();
-  } 
+  }
 }
